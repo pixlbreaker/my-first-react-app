367051bc63e3e4f20fd24efd7b19dbc16856c3a2
Michael Skotar <michaelskotar@gmail.com>
2025-06-26 15:37:02 -0400

    Inital repository

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = () => {
+  return (
+    <div>
+      <h2>Card Component</h2>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <h2>Function Arrow Component</h2>
+      <Card />
+
+    </div>
+  )
+}
+
+
+export default App
