3de58ef689be9b1341985ef0745fae51d3bcad88
Michael Skotar <michaelskotar@gmail.com>
2025-06-27 11:03:18 -0400

    Added useEffect Hooks for liking the movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
 
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
+
   return (
-    <div class="card">
-      <h2>{title}</h2>
+    <div class="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} - {count || null}</h2>
 
       <button onClick={() => {
         setHasLiked(!hasLiked)
